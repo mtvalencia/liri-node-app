ea340e006df65ad2fe616c9408b66443d5a01369
mtvalencia <mtv411@yahoo.com>
2019-05-19 16:16:12 -0600

    It works!

--- liri.js
+++ liri.js
@@ -8,11 +8,6 @@ var fs = require("fs");
 var sys = process.argv[2];
 var myInput = process.argv.slice(3).join(" ");
 
-// concert-this
-// spotify-this-song
-// movie-this
-// do-what-it-says
-
 if (sys === "concert-this") {
     bit();
 } else if (sys === "spotify-this-song") {
@@ -20,7 +15,7 @@ if (sys === "concert-this") {
 } else if (sys === "movie-this") {
     mov();
 } else if (sys === "do-what-it-says") {
-    spot2();
+    any();
 } else {
     console.log("You did not select an API to search on!");
 }
@@ -37,8 +32,20 @@ function bit() {
         }
     );
 }
+function bit2(myInput2) {
+    axios.get("https://rest.bandsintown.com/artists/" + myInput2 + "/events?app_id=codingbootcamp").then(
+        function (response) {
+            // Then we print out movie info
+            console.log("-----------------------------------------------");
+            console.log("Venue: " + response.data[0].venue.name);
+            console.log("Location: " + response.data[0].venue.city + ", " + response.data[0].venue.region);
+            console.log("Date of event: " + moment(response.data[0].datetime).format("MM-DD-YYYY"));
+            console.log("-----------------------------------------------");
+        }
+    );
+}
 
-function spot(myInput) {
+function spot() {
  if (!myInput) {
       myInput = "The Sign Ace of Base" ;
     }
@@ -55,8 +62,25 @@ function spot(myInput) {
   
     });
 }
+function spot2(myInput2) {
+    if (!myInput2) {
+         myInput2 = "The Sign Ace of Base" ;
+       }
+       spotify.search({ type: 'track', query: myInput2 }, function (err, data) {
+         if (err) {
+           return console.log('Error occurred: ' + err);
+         }
+         console.log("---------------------------------------------------");
+         console.log("Artist: " + data.tracks.items[0].artists[0].name)
+         console.log("Song: " + data.tracks.items[0].name);
+         console.log("Preview: " + data.tracks.items[3].preview_url);
+         console.log("Album: " + data.tracks.items[0].album.name);
+         console.log("---------------------------------------------------");
+     
+       });
+   }
 
-function mov(myInput) {
+function mov() {
     if (!myInput) {
         myInput = "Mr. Nobody";
     }
@@ -76,25 +100,45 @@ function mov(myInput) {
         }
     );
 }
+function mov2(myInput2) {
+    if (!myInput2) {
+        myInput2 = "Mr. Nobody";
+    }
+    axios.get("http://www.omdbapi.com/?t=" + myInput2 + "&y=&plot=short&apikey=trilogy").then(
+        function (response) {
+            // Then we print out movie info
+            console.log("-----------------------------------------------");
+            console.log("Title: " + response.data.Title);
+            console.log("Year: " + response.data.Year);
+            console.log("Rated: " + response.data.Rated);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+            console.log("-----------------------------------------------");
+        }
+    );
+}
 
-function spot2() {
+function any() {
     fs.readFile("random.txt", "utf8", function (error, data) {
         var arr = data.split(",");
         var sys = arr[0];
-        var myInput = arr[1].split('"').join("");
+        var myInput2 = arr[1].split('"').join("");
 
         if (error) {
           return console.log(error);
         }
     
         if (sys === "concert-this"){
-          bit(myInput);
+          bit2(myInput2);
         }
         if (sys === "spotify-this-song"){
-          spot(myInput);
+          spot2(myInput2);
         }
         if (sys === "movie-this"){
-          mov(myInput);
+          mov2(myInput2);
         }
     
       });
